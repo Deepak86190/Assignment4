661ccac77180004bc79fdf137c9c520938919acf
Deepak Maheshwari <115531844+Deepak86190@users.noreply.github.com>
2025-09-04 10:15:06 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,12 @@
+user_input = input("Enter some text to write to the file: ")
+with open('output.txt', 'w') as file:
+    file.write(user_input + '\n')
+
+additional_data = input("Enter additional text to append: ")
+with open('output.txt', 'a') as file:
+    file.write(additional_data + '\n')
+
+print("\nFinal content of output.txt:")
+with open('output.txt', 'r') as file:
+    for line in file:
+        print(line.rstrip())
